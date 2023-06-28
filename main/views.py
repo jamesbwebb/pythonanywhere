2d19a2dd1158629038ee5a11fa8bc7849d2cb995
James Webb <jamesbennettwebb@gmail.com>
2023-06-28 03:23:21 +0000

    Finished Templates and Custom HTML section

--- main/views.py
+++ main/views.py
@@ -6,10 +6,9 @@ from .models import ToDoList, Item
 def index(response, id):
 # This is a database query using the id# passed into the url
 	ls = ToDoList.objects.get(id=id)
-
 # for the HttpResponse object you can integrate html tags.
 # This is pretty cool because we are returning the database object attribute!
-	return render(response, "main/base.html", {"ls"=ls})
+	return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
 
