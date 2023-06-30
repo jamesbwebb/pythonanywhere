b4a3abdd312dcd40072da9e87a7af6e7e7b01636
James Webb <jamesbennettwebb@gmail.com>
2023-06-30 04:00:18 +0000

    Finished custom forms and added a sidebar

--- main/views.py
+++ main/views.py
@@ -7,6 +7,26 @@ from .forms import CreateNewList
 def index(response, id):
 # This is a database query using the id# passed into the url
 	ls = ToDoList.objects.get(id=id)
+
+	if response.method == "POST":
+# This checks to see which button was clicked to init the post request
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+# This checks the value of all check boxes and saves it into our database
+				if response.POST.get("c" + str(item.id)) == "clicked":
+					item.complete = True
+				else:
+					item.complete = False
+				item.save()
+# Here we take the text from the entry box and create an incomplete ToDoList item.
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("new")
+			if len(txt) > 2:
+				ls.item_set.create(text=txt, complete=False)
+			else:
+# This could be made more robust by adding something to validate the input.
+				print("invalid")
+
 # for the HttpResponse object you can integrate html tags.
 # This is pretty cool because we are returning the database object attribute!
 	return render(response, "main/list.html", {"ls":ls})
